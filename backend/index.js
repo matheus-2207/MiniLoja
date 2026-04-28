7dc33c49ddd467bc463d2bf81ea5167091019893
matheus-2207 <matheusvm2019@gmail.com>
2026-04-28 20:36:14 -0300

    Adicionando script de start para o Render

--- backend/index.js
+++ backend/index.js
@@ -251,7 +251,7 @@ app.get('/api/admin/stats/products', authenticate, isAdmin, (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
